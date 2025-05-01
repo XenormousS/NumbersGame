d3325b8a2b8bfd6fc27486dcb5b623ff9c0e74d7
XenormousS <whister83@gmail.com>
2025-05-01 05:32:17 +0300

    1.1.3

--- code.js
+++ code.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const nextBtn = document.getElementById("nextBtn");
-    const introScreen = document.getElementById("intro-screen");
-    const rulesScreen = document.getElementById("rules-screen");
-  
+  const nextBtn = document.getElementById("nextBtn");
+  const introScreen = document.getElementById("intro-screen");
+  const rulesScreen = document.getElementById("rules-screen");
+
+  if (nextBtn && introScreen && rulesScreen) {
     nextBtn.addEventListener("click", function () {
       introScreen.style.display = "none";
       rulesScreen.style.display = "block";
     });
-  });
+  } else {
+    console.error("Элемент не найден: проверь id в HTML");
+  }
+});
   
